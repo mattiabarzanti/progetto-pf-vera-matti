32fbcd261d77fa64cf74caff76364a3ff206317c
mattiab <mattia.barzanti@gmail.com>
2026-05-20 12:04:04 +0200

    ecco a te i comandi

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
-int main(){}
+int main(){
+    //git add nome file salva modifiche
+    //git commit -m "messaggio" fa il commit e manda messaggio
+    //git push mette su github "online"
+    //git pull lo prendi da git e vedi su pc
+    //git switch -c nome branch per creare
+    //git siwtch nome branch per cambiare
+    //controlla come fare per il merge della branch
+
+}
